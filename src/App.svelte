e337096345d2bf2a8b3f0d45dd0179041f5c46fa
David Leppik <david@leppik.net>
2021-07-28 15:26:00 -0500

    Updated text.

--- src/App.svelte
+++ src/App.svelte
@@ -19,14 +19,14 @@
     Changing the size of the court to any other fixed number changes the frequency of this lottery, but not its character. Within the limits of the Constitution, they only fair mechanism is to add justices at a fixed rate without regard to the court’s size. Adding one justice each presidential term gives voters, rather than chance or subterfuge, indirect control over the composition of the court.
 </p>
 <p>
-    Even if we could amend the Constitution to add term limits, that might not be wise. Judicial wisdom doers not peak at a particular age, and justices are often still improving when death or poor health force them to retire. Generational diversity is therefore best accomplished by keeping justices active as long as possible.
+    Even if we could amend the Constitution to add term limits, that might not be wise. Judicial wisdom does not peak at a particular age, and justices are often still improving when death or poor health force them to retire.
 </p>
 <p>
     There is no perfect size for the court. Some people prefer odd numbers to force a majority in every decision, but a tie vote (meaning the lower court's ruling stands) isn't an unreasonable outcome when the court is divided. The Court was founded with six members, and grew to 10 before being fixed at 9 in 1869.
 </p>
 <p>
     Different sizes have different advantages.
-    Size promotes diversity of thought, but if it is too large, a meaningful discussion becomes difficult. When I went to college, 16 was considered the ideal class size for discussions. My church has nine member Board of Trustees, based on the theory that larger boards encourage people to be wallflowers. Regardless, there is no single ideal size for decision making.
+    Size promotes diversity of thought, but if it is too large, a meaningful discussion becomes difficult. When I went to college, 16 was considered the ideal class size for discussions. My church has nine member Board of Trustees, based on the theory that larger boards encourage wallflowers. Different countries have a wide number of sizes. There is no single ideal size for decision making.
 </p>
 
 <p>
@@ -34,7 +34,9 @@
 </p>
 
 <p>
-    When justices are appointed around age 50 and retire around 85, we have a 9-member court 39% of the time, and it is between 7 and 10 members {39 + 25 +24 + 6}% of the time. <i>All of these are sizes that were explicitly chosen by Congress at one point or another.</i> But perhaps the best argument for eliminating a fixed size is what happens if the entire Court is killed in a terrible accident. Under the present system, the current president's choices would dominate for the next thirty years. With fixed-rate appointments, the court would be small but diverse—and could recover quicker with emergency rules.
+    When justices are appointed around age 50 and retire around 85, we have a 9-member court 39% of the time, and it is between 7 and 10 members {39 + 25 +24 + 6}% of the time. In other words, <i>fixed-rate appointments might not change the court's size very much.</i> </p>
+
+<p>Perhaps the best argument for eliminating a fixed size is what happens if the entire Court is killed in a terrible accident. Under the present system, the current president's choices would dominate for the next thirty years. With fixed-rate appointments, the court would be small but diverse—and could recover quicker with emergency rules, such as allowing slightly more appointments to a small court.
 </p>
 
 <p>
